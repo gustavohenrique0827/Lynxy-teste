46f38326e029ad8312d012350dbdd7cd8cea2c51
Gustavo Henrique <portilhogustavohenriquegodoi@gmail.com>
2025-10-19 15:34:51 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? "/Lynxy-teste/" : "/",
+  base: mode === 'production' ? "/Lynxy-teste" : "/",
   server: {
     host: "::",
     port: 8080,
